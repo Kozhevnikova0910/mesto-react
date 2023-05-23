b711191ab65598cae9b098db39d49f266ec22287
Kozhevnikova0910 <kozhevnikova0910@gmail.com>
2023-05-23 14:59:19 +0200

    fix: 11sprint review

--- src/components/EditProfilePopup.js
+++ src/components/EditProfilePopup.js
@@ -30,7 +30,7 @@ function EditProfilePopup(props) {
     React.useEffect(() => {
         setName(currentUser.name);
         setDescription(currentUser.about);
-    }, [currentUser])
+    }, [props.isOpen])
 
     return (
         <PopupWithForm name="edit-profile"
